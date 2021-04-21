3a008acbd867248ab573ffcc9eb5e9165a3e6117
BrandonS <bsmith@frontporch.com>
2021-04-21 15:04:42 -0700

    DO-2700 Create Build Process (#2)
    * DO-2700 Create Build Process
    
    * Remove Old Build Script
    
    * Update Readme

--- build.py
+++ build.py
@@ -2,28 +2,36 @@
 # If changes are needed to the font, open the font .sfd in FontForge and make them there
 # This script currently is only used to generate the font
 
+print("Importing modules...")
 import os
 import datetime
 import platform
 import fontforge as ff
 
+print("Setting variables...")
 MAIN_DIR            = os.path.dirname(os.getcwd())
 CURRENT_DIR         = os.getcwd()
-OUTPUT_PATH         = MAIN_DIR + "\published"
-SCRIPTS_PATH        = CURRENT_DIR + "\scripts"
+OUTPUT_PATH         = CURRENT_DIR + "/published/"
+SCRIPTS_PATH        = CURRENT_DIR
 FONT_NAME_LIST      = ["Bold", "BoldItalic", "Italic", "Regular"]
 FONT_FAMILY_NAME    = "Roboto-GMM"
 
+print("Starting build process...")
 if not os.path.exists(OUTPUT_PATH):
     os.mkdir(OUTPUT_PATH)
+
+print(CURRENT_DIR)
 
 def generate_fonts() :
     print("FontForge version: " + ff.version())
     print("Python version: "+ platform.python_version())
     for name in FONT_NAME_LIST:
         font_name     = FONT_FAMILY_NAME + "-" + name
-        project_file  = CURRENT_DIR + "/" + font_name + ".sfd"
-        font_file     = OUTPUT_PATH + "/" + font_name + ".ttf"
+        print(font_name)
+        project_file  = CURRENT_DIR + "/src/" + font_name + ".sfd"
+        print(project_file)
+        font_file     = OUTPUT_PATH + font_name + ".ttf"
+        print(font_file)
         _generate_font(project_file, font_file, font_name)
 
 def _generate_font(project_file, font_file, font_name):
