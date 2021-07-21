db8d4d53a236a8cf32932d4394b20246f01bdf8d
Kalley Powell <kpowell@frontporch.com>
2021-07-21 09:14:32 -0500

    GPA-7074 Add CSS file, woff, and woff2 formats (#4)
    * GPA-7074 Add CSS file, woff, and woff2 formats
    
    * GPA-7074 Move template higher up

--- build.py
+++ build.py
@@ -15,6 +15,30 @@ OUTPUT_PATH         = CURRENT_DIR + "/published/"
 SCRIPTS_PATH        = CURRENT_DIR
 FONT_NAME_LIST      = ["Bold", "BoldItalic", "Italic", "Regular"]
 FONT_FAMILY_NAME    = "Roboto-GMM"
+FILE_TYPES          = ["ttf", "woff", "woff2"]
+FONT_STYLE_MAPS     = {
+                        "Bold": {
+                            "weight": 700
+                        },
+                        "BoldItalic": {
+                            "style": "italic",
+                            "weight": 700
+                        },
+                        "Italic": {
+                            "style": "italic"
+                        },
+                        "Regular": {}
+                    }
+FONT_FACE_TEMPLATE = '''@font-face {{
+  font-family: Roboto;
+  font-style: {style};
+  font-display: swap;
+  font-weight: {weight};
+  src: local("Roboto-GMM"), url("./{font_name}.woff2") format("woff2"),
+    url("./{font_name}.woff") format("woff"),
+    url("./{font_name}.ttf") format("truetype");
+}}
+'''
 
 print("Starting build process...")
 if not os.path.exists(OUTPUT_PATH):
@@ -22,17 +46,20 @@ if not os.path.exists(OUTPUT_PATH):
 
 print(CURRENT_DIR)
 
-def generate_fonts() :
+
+def generate_fonts():
     print("FontForge version: " + ff.version())
-    print("Python version: "+ platform.python_version())
+    print("Python version: " + platform.python_version())
     for name in FONT_NAME_LIST:
         font_name     = FONT_FAMILY_NAME + "-" + name
         print(font_name)
         project_file  = CURRENT_DIR + "/src/" + font_name + ".sfd"
         print(project_file)
-        font_file     = OUTPUT_PATH + font_name + ".ttf"
-        print(font_file)
-        _generate_font(project_file, font_file, font_name)
+        for ext in FILE_TYPES:
+            font_file     = OUTPUT_PATH + font_name + "." + ext
+            print(font_file)
+            _generate_font(project_file, font_file, font_name)
+
 
 def _generate_font(project_file, font_file, font_name):
     font = ff.open(project_file)
@@ -41,8 +68,25 @@ def _generate_font(project_file, font_file, font_name):
     print(datetime.datetime.now().strftime('[%Y-%m-%d %H:%M:%S.%f]')
           + " '" + font_name + "' " + "generated.")
 
+
+def generate_css():
+    print("Generating CSS File")
+    
+    with open(OUTPUT_PATH + "roboto-gmm.css", "w+") as f:
+        for name in FONT_NAME_LIST:
+            font_name = FONT_FAMILY_NAME + "-" + name
+            style_map = FONT_STYLE_MAPS[name]
+            style = style_map.get("style", "normal")
+            weight = style_map.get("weight", 400)
+            print("writing @font-face for " + font_name)
+            f.write(FONT_FACE_TEMPLATE.format(style=style,
+                    weight=weight, font_name=font_name))
+
+
 def main():
     generate_fonts()
+    generate_css()
+
 
 if __name__ == "__main__":
     main()
